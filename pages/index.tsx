31164e3331e4f4088c40a97efb1528efc987d661
Junho Park <dwarfthema@gmail.com>
2022-09-11 12:23:54 +0900

    controller fix#1

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,35 +4,32 @@ import { Unity, useUnityContext } from "react-unity-webgl";
 
 const Home: NextPage = () => {
   const { unityProvider } = useUnityContext({
-    loaderUrl: "Build/tesxNext.loader.js",
-    dataUrl: "Build/tesxNext.data",
-    frameworkUrl: "Build/tesxNext.framework.js",
-    codeUrl: "Build/tesxNext.wasm",
+    loaderUrl: "Build/testNext.loader.js",
+    dataUrl: "Build/testNext.data",
+    frameworkUrl: "Build/testNext.framework.js",
+    codeUrl: "Build/testNext.wasm",
   });
   return (
-    <>
-      <div
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        backgroundColor: "black",
+        height: "100%",
+        width: "100%",
+      }}
+    >
+      <h1>WebGl Test</h1>
+      <Unity
         style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
+          height: "90%",
+          width: "100%",
         }}
-      >
-        <h1>WebGl Test</h1>
-        <Unity
-          style={{
-            height: "90%",
-            width: "100%",
-          }}
-          unityProvider={unityProvider}
-        />
-      </div>
-      <style jsx>{`
-        div {
-        }
-      `}</style>
-    </>
+        unityProvider={unityProvider}
+      />
+    </div>
   );
 };
 
